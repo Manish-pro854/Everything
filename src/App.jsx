3d5e0c6734a9ea5296b7160b7fe472fc904154b1
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-06-23 22:03:30 +0530

    set up for making my everything mall website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='h-screen bg-amber-400 text-9xl text-center'>Minku Don</h1>
+    </div>
+  )
+}
+
+export default App
