7b2d43d20148edcee00fe254f791471cedfba8d9
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-06-28 17:27:16 +0530

    stat making cart section

--- src/Components/Pages/Location.jsx
+++ src/Components/Pages/Location.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import map from "../../assets/map.png"
 
 const Location = () => {
   return (
-    <div>
-      
+    <div id='map' className="pt-40 md:pt-40 lg:pt-21 flex flex-col gap-5 items-center justify-center mb-10">
+      <h1 className="text-center text-5xl md:text-6xl pb-5">Center Map</h1>
+      <div className="h-screen md:w-[90vw] px-2">
+      <img src={map} className='h-full w-full' />
+      </div>
     </div>
   )
 }
